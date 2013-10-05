b1ac68482a418da6bf5b7aa3f90e5cb3f3d64a42
annelu <annelu@notalion.com>
2013-10-05 12:18:43 -0700

    stringifyJSON now functional

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,37 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+	if (typeof obj === "number") {
+		return obj.toString();
+	} else if (Object.prototype.toString.call(obj) === '[object Object]') {
+	var result = "";
+	for (key in obj) {
+		if (obj[key] === undefined || obj[key] === "function") {
+			return "{}";
+		} else if (result) {
+			result = result + "," + stringifyJSON(key) + ":" + stringifyJSON(obj[key]);
+		} else {
+			result = result + stringifyJSON(key) + ":" + stringifyJSON(obj[key]);	  			
+		}
+	}
+		return "{" + result + "}";
+	} else if (Object.prototype.toString.call(obj) === '[object Null]') {
+		return "null";
+	} else if (Object.prototype.toString.call(obj) === '[object Array]') {
+		var result = "";
+		for (i = 0; i < obj.length; i++) {
+			if (result) {
+			var result = result + "," + stringifyJSON(obj[i]);  				
+			} else {
+				var result = stringifyJSON(obj[i]);
+			}
+		}
+		return "[" + result +"]";
+	} else if (typeof obj === "string") {
+	  	return '"' + obj + '"';
+	} else if (typeof obj === "boolean") {
+		return obj.toString();
+	} else {
+		return undefined;
+	}	
 };
